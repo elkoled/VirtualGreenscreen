62d3ef67c0f332caf6089fb884b1880c8682e44b
Daniel Köpping <daniel.koepping@de.abb.com>
2021-02-17 00:53:53 +0100

    added debug printouts

--- virtual_greenscreen.py
+++ virtual_greenscreen.py
@@ -7,7 +7,8 @@ import pyvirtualcam
 import torch
 from torch.autograd import Variable
 
-from model import U2NET # full size version 173.6 MB
+from model import U2NET,U2NETP # full size version 173.6 MB
+#from model import U2NETP # small size version 4.7MB
 
 # set webcam resolution and target fps
 # high resolutions requires much VRAM
@@ -15,21 +16,19 @@ width = 1280
 height = 720
 target_fps = 30
 
-# inference resolution, 1.0 = full image resolution
-inference_resolution = 0.3
-
-# fps average samples
-fps_avg_samples = 10
+# inference scale, 1.0 = full image resolution used for inference
+inference_scale = 0.3
 
 
 def main():
     checkCuda()
 
-    ###### 1. set model path ######
-    model_name='u2net'
+    #TODO Make model selectable in cmd
+    ###### 1. select model ######
+    model_name='u2net_human_seg'
+    #model_name='u2netp'
     
-    model_dir = os.path.join(os.getcwd(), 'pretrained', model_name+'_human_seg', model_name + '_human_seg.pth')
-    #model_dir = os.path.join(os.getcwd(), 'pretrained', model_name+'p', model_name + 'p.pth')
+    model_dir = os.path.join(os.getcwd(), 'pretrained', model_name, model_name + '.pth')
 
     ###### 2. setup webcam ######
     cap = cv2.VideoCapture(0)
@@ -41,9 +40,12 @@ def main():
     cap.set(4, height)
 
     ###### 3. load pretrained model ######
-    if(model_name=='u2net'):
+    if(model_name=='u2net_human_seg'):
         print("...load U2NET---173.6 MB")
         net = U2NET(3,1)
+    elif(model_name=='u2netp'):
+        print("...load U2NEP---4.7 MB")
+        net = U2NETP(3,1)
 
     if torch.cuda.is_available():
         net.load_state_dict(torch.load(model_dir))
@@ -64,7 +66,7 @@ def main():
             # frame[:,:,3] = 255
             new_frame_time = time.time()
             ret, webcam_frame = cap.read()
-            frame = cv2.resize(webcam_frame, None, fx=inference_resolution, fy=inference_resolution, interpolation=cv2.INTER_AREA)
+            frame = cv2.resize(webcam_frame, None, fx=inference_scale, fy=inference_scale, interpolation=cv2.INTER_AREA)
 
             # start of Inference
             frame = np.array(frame).astype(np.float32) / 255.  # Scale
@@ -93,11 +95,13 @@ def main():
             predict = predict.squeeze()
             predict_np = predict.cpu().data.numpy()
 
+            print("predict: %.2fms"%((time.time()-new_frame_time)*1000))
+            
             # upscale of predicted mask
             im = Image.fromarray(predict_np*255).convert('RGB')
             mask = im.resize(image_shape,resample=Image.BILINEAR)
             mask = np.array(mask)
-
+            
             # generate green screen
             background = np.full(webcam_frame.shape, (0,255,0), dtype=np.uint8)
 
@@ -113,6 +117,7 @@ def main():
 
             cv2.imshow('Input', outImage)
             # end of inference
+            print("predict and multiply: %.2fms"%((time.time()-new_frame_time)*1000))
 
             # Calc and print FPS every 0.5s
             fps = 1/(new_frame_time-prev_frame_time)
@@ -123,9 +128,15 @@ def main():
             #frame_web = np.zeros((cam.height, cam.width, 4), np.uint8) # RGBA
             #frame_web[:,:,:3] = cam.frames_sent % 255 # grayscale animation
             #frame_web[:,:,3] = 255
-            
-            #cam.send(outImage)
-            #cam.sleep_until_next_frame()
+
+            #TODO Write to virtual camera
+            #info = np.info(outImage.dtype)
+            # outImage = outImage.astype(np.float64)
+            # outImage = 255 * outImage
+            # img = outImage.astype(np.uint8)
+
+            # cam.send(img)
+            # cam.sleep_until_next_frame()
 
             c = cv2.waitKey(1)
             if c == 27:
